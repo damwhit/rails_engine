6de5e256e6e5465f3e01c9db81b5438d17c8b134
David Whitaker <damwhit@gmail.com>
2016-04-12 14:51:02 -0600

    index and show methods tested for transaction

---
+++ spec/requests/api/v1/transactions/transaction_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe "GET api/v1/transactions/:id" do
+  it "returns a specific transaction" do
+    transaction1 = create(:transaction)
+    create(:transaction, credit_card_number: "424242424242424242",
+                      invoice_id: nil,
+                      result: "guud",
+                      created_at: "2012-03-26 14:53:59 UTC",
+                      updated_at: "2012-03-26 14:53:59 UTC")
+    create(:transaction, credit_card_number: "424242424242424242",
+                      invoice_id: nil,
+                      result: "guud",
+                      created_at: "2012-03-25 14:53:59 UTC",
+                      updated_at: "2012-03-25 14:53:59 UTC")
+
+    get "/api/v1/transactions/#{transaction1.id}"
+
+    expect(response.status).to eq(200)
+    expect(json_body.count).to eq(6)
+
+    expect(json_body).to eq({
+      id: transaction1.id,
+      credit_card_number: transaction1.credit_card_number,
+      invoice_id: nil,
+      result: "good",
+      created_at: "2012-03-27T14:53:59.000Z",
+      updated_at: "2012-03-27T14:53:59.000Z"
+      })
+  end
+end
---
+++ spec/requests/api/v1/transactions/transactions_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe "GET api/v1/transactions" do
+  it "returns a list of all transactions" do
+    transaction1 = create(:transaction)
+    create(:transaction, credit_card_number: "424242424242424242",
+                      invoice_id: nil,
+                      result: "guud",
+                      created_at: "2012-03-26 14:53:59 UTC",
+                      updated_at: "2012-03-26 14:53:59 UTC")
+    create(:transaction, credit_card_number: "424242424242424242",
+                      invoice_id: nil,
+                      result: "guud",
+                      created_at: "2012-03-25 14:53:59 UTC",
+                      updated_at: "2012-03-25 14:53:59 UTC")
+
+    get "/api/v1/transactions"
+
+    expect(response.status).to eq(200)
+    expect(json_body.count).to eq(3)
+
+    transaction1_json = json_body[0]
+    expect(transaction1_json).to eq({
+      id: transaction1.id,
+      credit_card_number: transaction1.credit_card_number,
+      invoice_id: nil,
+      result: "good",
+      created_at: "2012-03-27T14:53:59.000Z",
+      updated_at: "2012-03-27T14:53:59.000Z"
+      })
+  end
+end
