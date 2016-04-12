f2172321bc4895dd971291ae19828f371e6774f9
David Whitaker <damwhit@gmail.com>
2016-04-12 10:09:02 -0600

    item model is now tested

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ActiveRecord::Base
   belongs_to :merchant
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :unit_price, presence: true
+  validates :created_at, presence: true
+  validates :updated_at, presence: true
 end
